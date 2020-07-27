b753814e5154c03fc58268859dc44c68bb2f4db2
Ajay Jayan <ajay.jayan051@gmail.com>
2020-07-27 23:00:33 +0530

    deploy

---
+++ 4.2 robots.js.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Akash Shan',
+    username: 'Kutty Shan',
+    email: 'KuttyShan@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Ajay Kumar',
+    username: 'AK',
+    email: 'ajaykumar@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Akash E',
+    username: 'Rockybhai',
+    email: 'akashe@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Amaresh Guhan',
+    username: 'Trendy',
+    email: 'Amaresh.Guhan@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Rahul U',
+    username: 'Rocket',
+    email: 'Rahul_U@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'MSS Jayarjh',
+    username: 'Ganesh',
+    email: 'Jay_rajh@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Mukesh S',
+    username: 'Sweetheart(tamil)',
+    email: 'Mukesh.S@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Soorya Raj',
+    username: 'Apartment',
+    email: 'Sendnudessoorya@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Barath Kumar AA',
+    username: 'ActorBU',
+    email: 'Barath_BU@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Ajay Jayan',
+    username: 'ajaay_jaay',
+    email: 'ajay.jayan051@gmail.com'
+  }
+];
